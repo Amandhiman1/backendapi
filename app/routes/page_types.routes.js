467d86c959140e0bf597fd16f7a129207f29f4b6
Ram <team@teqtop.com>
2024-12-31 14:43:48 +0530

    show pages link

--- app/routes/page_types.routes.js
+++ app/routes/page_types.routes.js
@@ -6,28 +6,28 @@ module.exports = app => {
   var router = require("express").Router();
 
   // Create a new Tutorial
-  router.post("/", page_types.create);
+  router.post("/", authenticateToken, page_types.create);
   // Create a new Tutorial
-  router.post("/filter", page_types.filter);
+  router.post("/filter", authenticateToken, page_types.filter);
   // Retrieve all page_types
-  router.get("/", page_types.findAll);
+  router.get("/", authenticateToken, page_types.findAll);
   
   // Retrieve all published page_types
-  router.get("/published", page_types.findAllPublished);
-  router.get("/allcount", page_types.allcount);
+  router.get("/published", authenticateToken, page_types.findAllPublished);
+  router.get("/allcount", authenticateToken, page_types.allcount);
 
   // Retrieve a single Tutorial with id
-  router.get("/:id", page_types.findOne);
+  router.get("/:id", authenticateToken, page_types.findOne);
   
   // Update a Tutorial with id
-  router.put("/:id", page_types.update);
-  router.put("/add-data/:id", page_types.addData);
+  router.put("/:id", authenticateToken, page_types.update);
+  router.put("/add-data/:id", authenticateToken, page_types.addData);
 
   // Delete a Tutorial with id
-  router.delete("/:id", page_types.delete);
+  router.delete("/:id", authenticateToken, page_types.delete);
 
   // Create a new Tutorial
-  router.delete("/", page_types.deleteAll);
+  router.delete("/", authenticateToken, page_types.deleteAll);
 
   app.use("/api/page_types", router);
 };
